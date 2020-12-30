fb0b3888c06ff10d0814fe0d3f3963dc1e7bd88a
foggynight <foggynight709@gmail.com>
2020-12-30 05:38:49 -0330

    Add initial window

---
+++ src/main.c
@@ -0,0 +1,32 @@
+/* --- quark ---
+ * Copyright (C) 2020 Robert Coffey
+ * Released under the GPLv2 license */
+
+#include <stdlib.h>
+
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow *win;
+
+    if (!glfwInit())
+        return EXIT_FAILURE;
+
+    win = glfwCreateWindow(640, 480, "Hello, World.", NULL, NULL);
+    if (!win) {
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(win);
+
+    while (!glfwWindowShouldClose(win)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(win);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return EXIT_SUCCESS;
+}
